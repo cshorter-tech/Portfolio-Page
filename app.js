f0b83f1dd0024d756264eed3df240b81f38bfa57
cshorter-tech <13804036+cshorter-tech@users.noreply.github.com>
2020-09-20 02:51:48 -0400

    Static content and images added to index

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static('public'));
+app.use('images', express.static(__dirname + 'public/images'));
+
 app.get('/', function (req, res) {
-    res.send('Hello World - Intro')
+    res.sendFile('./views/index.html', { root: __dirname, index: false, extensions: ['html'] })
 });
 
 app.get('/portfolio', function (req, res) {
-    res.send('This is my portfolio page')
+    res.sendFile('./views/projects.html', { root: __dirname, index: false, extensions: ['html'] })
 });
 
 app.get('/blog', function (req, res) {
-    res.send('This is my blog page')
+    res.sendFile('./views/blog.html', { root: __dirname, index: false, extensions: ['html'] })
 });
 
 app.get('/contact', function (req, res) {
-    res.send('This is my contact page')
+    res.sendFile('./views/contact.html', { root: __dirname, index: false, extensions: ['html'] })
 });
+
+
 
 app.listen(3000, () => {
     console.log('Local server started on port 3000.')
